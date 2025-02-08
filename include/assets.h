ccbb347669d776f5831834bd49a8100405dfff5c
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-08 11:33:46 +0100

    feat: add load_sprite function to load a sprite from a file

---
+++ include/assets.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** Project - La Ilusion
+** File description:
+** Header file for all things
+** related to asset management
+*/
+
+#ifndef CORE_ASSETS_H
+    #define CORE_ASSETS_H
+    #include <SFML/Graphics.h>
+
+
+sfSprite *load_sprite(const char *path);
+
+
+#endif
