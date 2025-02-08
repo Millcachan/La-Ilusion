7ee0a77cd37207d15779b56d035ca8364ca7598b
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-08 14:01:59 +0100

    feat!: Started working on platform

---
+++ include/object/platform.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Project - La Ilusion
+** File description:
+** Header file for all things
+** related to platforms
+*/
+
+#ifndef OBJECT_PLATFORM_H
+    #define OBJECT_PLATFORM_H
+    #include <SFML/Graphics.h>
+
+
+struct platform_s {
+    sfSprite *sprite;
+    float accel_x;
+};
+
+
+#endif
