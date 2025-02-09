0bbb2c6951373db6a4c04ab4e5587f26f164d8fb
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-09 16:29:54 +0100

    fix(scene/ingame): add missing free calls to free_ingame

--- src/scene/ingame/free_ingame.c
+++ src/scene/ingame/free_ingame.c
@@ -7,6 +7,7 @@
 */
 
 #include "game.h"
+#include "object/player.h"
 
 
 void free_ingame(game_t *game)
@@ -17,11 +18,24 @@ void free_ingame(game_t *game)
     scene_ingame_t *data = game->scene->data;
     if (!data)
         return;
-    
+
+    // Free background:
     for (int i = 0; data->background[i]; i++) {
         sfTexture_destroy(data->background_texture[i]);
         sfSprite_destroy(data->background[i]);
     }
+
+    // Free player:
+    sfSprite_destroy(data->player->sprite);
+    for (int i = 0; i < 3; i++)
+        sfTexture_destroy(data->player_textures[i]);
+    free(data->player);
+
+    // Free platforms:
+    for (int i = 0; i < 20; i++)
+        sfSprite_destroy(data->platforms[i]->sprite);
+    sfTexture_destroy(data->platform_textures[0]);
+    sfTexture_destroy(data->platform_textures[1]);
 
     sfText_destroy(data->timer_text);
     sfFont_destroy(data->timer_font);
